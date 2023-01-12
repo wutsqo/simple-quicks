5c8811cdae5e7c33596c7a9626958f2cb8863f1b
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2023-01-13 05:01:35 +0700

    refactor: remove code smells

--- components/QuickTabs/QuickTabWrapper.tsx
+++ components/QuickTabs/QuickTabWrapper.tsx
@@ -13,7 +13,7 @@ export const QuickTabWrapper: FC<QuickTabWrapperProps> = ({
   active,
   translateTo,
 }) => {
-  const hideTitle = !(state === "expanded")
+  const hideTitle = state !== "expanded"
 
   return (
     <div
@@ -33,8 +33,7 @@ export const QuickTabWrapper: FC<QuickTabWrapperProps> = ({
       <button
         title="Close"
         className={classNames(
-          "h-quick-start w-quick-start bg-dark-gray rounded-full absolute\
-          transition-all duration-300 ease-in-out",
+          "h-quick-start w-quick-start bg-dark-gray rounded-full absolute transition-all duration-300 ease-in-out",
           active ? "-translate-x-4 scale-100" : "translate-x-0 scale-90"
         )}
         onClick={onClose}
@@ -42,8 +41,7 @@ export const QuickTabWrapper: FC<QuickTabWrapperProps> = ({
 
       <button
         className={classNames(
-          "rounded-full bg-white flex justify-center items-center\
-          transition-all duration-300 ease-in-out h-quick-start w-quick-start",
+          "rounded-full bg-white flex justify-center items-center transition-all duration-300 ease-in-out h-quick-start w-quick-start",
           active ? activeClassname : inactiveClassname,
           active ? "scale-100" : "scale-90"
         )}
